076495ff30e7ab42062f4101684695bf27ff66a6
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-07 14:20:10 -0500

    Create Handlepost.php

---
+++ Handlepost.php
@@ -0,0 +1,6 @@
+<?php require_once("Header.php"); ?>
+
+<h1> Post response</h1>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<a class – “btn btn-primary” href-</a>
