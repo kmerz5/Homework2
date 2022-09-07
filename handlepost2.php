dc6a2d40f54cae7699c6b0db73a701eb7a71b99e
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-07 14:34:26 -0500

    Update handlepost2.php

--- handlepost2.php
+++ handlepost2.php
@@ -1,6 +1,6 @@
 <?php require_once("Header.php"); ?>
 
 <h1> Get response</h1>
-Welcome <?php echo $_POST["name"]; ?><br>
+Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 <a class – “btn btn-primary” href-</a>
