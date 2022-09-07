441e2bad9264be7e2d4d7281dfdd37230134c798
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-07 14:28:28 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,1 +1,14 @@
 
+<?php require_once("Header.php"); ?>
+<h1> My website</h1>
+
+<form action="handlepost2.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
